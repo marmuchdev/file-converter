63710ff924ccaebabffea59479fad68ce5424e00
marmuchdev <marmuchdev@gmail.com>
2025-06-29 15:08:35 +0100

    add hibob-to-pdf

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 from .converters.json_to_pdf import json_to_pdf_bp
+from .converters.hibob_to_pdf import hibob_to_pdf_bp
 
 def create_app():
     app = Flask(__name__)
     CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "http://frontend:80"]}})
     app.register_blueprint(json_to_pdf_bp, url_prefix='/api')
+    app.register_blueprint(json_to_pdf_bp,url_prefix='/api')
     return app
